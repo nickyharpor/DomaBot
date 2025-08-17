96632f73a0cf4e78316986d023ff4ca916479f71
Mehraz <info@mehrazplus.com>
2025-08-17 10:59:59 +0000

    project skeleton

---
+++ bot.py
@@ -0,0 +1,41 @@
+from telethon import TelegramClient, events
+import os
+from dotenv import load_dotenv
+import polib
+
+
+
+# Environment detection
+load_dotenv()
+env = os.getenv("ENV")
+if env == 'live':
+    import config_live
+    config = config_live
+elif env == 'dev':
+    import config_dev
+    config = config_dev
+else:
+    import config_test
+    config = config_test
+
+# Load the message file
+msg = {}
+for entry in polib.pofile('msg_' + config.language + '.po'):
+    msg[entry.msgid] = entry.msgstr
+
+# Connect to database
+# db = Mongo(config.db_host, config.db_port, config.db_name)
+
+# Initialize Telegram client
+if config.proxy:
+    bot = TelegramClient(session=config.session_name, api_id=config.api_id, api_hash=config.api_hash,
+                         proxy=(config.proxy_protocol, config.proxy_host, config.proxy_port))
+else:
+    bot = TelegramClient(session=config.session_name, api_id=config.api_id, api_hash=config.api_hash)
+
+
+@bot.on(events.NewMessage(pattern='/start', incoming=True))
+@bot.on(events.CallbackQuery(data=b'main_menu'))
+async def start(event):
+    if event.sender_id in config.admin_list:
+        pass
---
+++ config_test.py
@@ -0,0 +1,12 @@
+language='en'
+
+api_id=0
+api_hash=''
+session_name=''
+
+proxy=False
+proxy_protocol=''
+proxy_host=''
+proxy_port=0
+
+admin_list=[]
